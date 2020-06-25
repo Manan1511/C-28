7ca2b4e1824e2dedf6f4db8b206214395e515247
Manan1511 <mananshah1511@gmail.com>
2020-06-25 17:36:43 +0530

    hi w q wq ~wq wwwqwq w

--- Chain.js
+++ Chain.js
@@ -1,20 +1,24 @@
 class Chain{
-    constructor(bodyA, bodyB){
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
-            stiffness: 0.04,
-            length: 10
+            pointB: pointB,
+            stiffness: 0.09,
+            length: 10,
         }
         this.chain = Constraint.create(options);
         World.add(world, this.chain);
     }
 
     display(){
+        if(this.chain.bodyA){
         var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
+        var pointB = this.chain.pointB;
         strokeWeight(4);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
     }
-    
+    }
+    fly(){
+        this.chain.bodyA=null;
+    }
 }
